19c378daa4f57bb8b609faa1b440b64ae476053c
Ken Kochis <kkochis@outlook.com>
2019-12-07 10:07:53 -0800

    Update for VS2019

--- build_windows.bat
+++ build_windows.bat
@@ -4,7 +4,7 @@ RMDIR /S /Q build64
 MKDIR build64
 PUSHD build64
 
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC_LIBS=ON ..
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" libebur128.sln
+cmake -G "Visual Studio 16 2019" -DBUILD_STATIC_LIBS=ON ..
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" libebur128.sln
 
 POPD build64
