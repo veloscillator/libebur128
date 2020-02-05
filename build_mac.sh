313c3751285f9ba4c3a54e5ca1fe1ed943b30332
Ken Kochis <ken@pex.com>
2020-02-05 13:52:31 -0800

    Fix mac build for debug/release

--- build_mac.sh
+++ build_mac.sh
@@ -2,7 +2,8 @@ rm -rf build64
 mkdir build64
 cd build64
 
-cmake -G Xcode -DBUILD_STATIC_LIBS=ON ..
-xcodebuild
+cmake -G Xcode -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF ..
+xcodebuild -configuration Debug
+xcodebuild -configuration Release
 
 cd ..
